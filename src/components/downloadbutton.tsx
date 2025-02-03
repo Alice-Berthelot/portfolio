1d2f45fae516448fac2ee9a2294284d7239dbdb6
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-02-03 21:43:45 +0100

    modified resume buttons design

--- src/components/downloadbutton.tsx
+++ src/components/downloadbutton.tsx
@@ -11,7 +11,7 @@ export default function DownloadButton({ title, source }: DownloadButtonProp) {
       href={source}
       target="_blank"
       aria-label="Ouvrir le CV dans un nouvel onglet"
-      className="relative z-30 inline-flex items-center justify-center px-6 py-3 text-sm font-bold text-ghost-white rounded-full cursor-pointer shadow-[0_2px_4px_rgba(0,0,0,1),0_10px_20px_rgba(0,0,0,0.4)] transition-all duration-200 ease-out bg-gradient-to-b from-[#171717] to-[#242424] border border-[#292929] hover:translate-y-0.5 hover:shadow-[0_1px_2px_rgba(0,0,0,1),0_5px_10px_rgba(0,0,0,0.4)] before:absolute before:top-[-2px] before:right-[-1px] before:bottom-[-1px] before:left-[-1px] before:bg-gradient-to-b before:from-[#292929] before:to-[#000000] before:rounded-full before:z-[-1] before:transition-all before:duration-200"
+      className="inline-block flex items-center justify-center gap-2 py-4 px-6 rounded-md border border-solid border-ghost-white/30 text-sm font-bold download-button hover:dark-charcoal"
     >
       {title} <IoOpenOutline className="text-xl" />
     </a>
