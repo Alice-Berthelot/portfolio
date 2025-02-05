8b6391ec5c742267b5a05f740de3881d3dd90365
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-02-05 23:01:49 +0100

    modified logics for background page, to be more specific for view details button + added some background data

--- src/components/backgroundhistory.tsx
+++ src/components/backgroundhistory.tsx
@@ -13,17 +13,23 @@ export default function BackgroundHistory({
   more,
   less,
 }: HistoryProps) {
-  const [detailedItems, setDetailedItems] = useState<{ [key: number]: boolean }>({});
+  const [detailedItems, setDetailedItems] = useState<{ [key: string]: { [index: number]: boolean } }>({});
 
-  const handleToggle = (descriptionIndex: number) => {
-    setDetailedItems((prevState) => ({
-      ...prevState,
-      [descriptionIndex]: !prevState[descriptionIndex], 
-    }));
+  const handleToggle = (backgroundKey: string, descriptionIndex: number) => {
+    setDetailedItems((prevState) => {
+      const backgroundDetails = prevState[backgroundKey] || {}; // get details for this background or {}
+      return {
+        ...prevState,
+        [backgroundKey]: {
+          ...backgroundDetails,
+          [descriptionIndex]: !backgroundDetails[descriptionIndex], // Toggle only for the index of this background
+        },
+      };
+    });
   };
 
   return (
-    <section className="pt-16 flex flex-col gap-8">
+    <section className="pt-16 pb-16 flex flex-col gap-8">
       {backgroundData.map((background) => (
         <article key={background.key}>
           <h4 className="font-title text-2xl tracking-wide mx-10 mb-3">
@@ -35,35 +41,27 @@ export default function BackgroundHistory({
                 {description.value}
                 {Array.isArray(description.details) &&
                   description.details.length > 0 &&
-                  description.details.every(
-                    (item) => typeof item === "string"
-                  ) && (
+                  description.details.every((item) => typeof item === "string") && (
                     <>
-          
-                      {!detailedItems[index] && (
+                      {!detailedItems[background.key]?.[index] ? (
                         <button
                           className="ml-6 underline underline-offset-2"
-                          onClick={() => handleToggle(index)} // Sending to handleToggle the description index corresponding to the description of which we WANT details
+                          onClick={() => handleToggle(background.key, index)} // Sending both background.key and details index to ensure the button corresponds to the right li
                         >
                           {more}
                         </button>
-                      )}
-
-                      {detailedItems[index] && (
+                      ) : (
                         <>
                           <ul className="mt-2">
-                            {description.details.map((detail, index) => (
-                              <li
-                                key={index}
-                                className="text-sm ml-4 list-disc"
-                              >
+                            {description.details.map((detail, i) => (
+                              <li key={i} className="text-sm ml-4 list-disc">
                                 {detail}
                               </li>
                             ))}
                           </ul>
                           <button
                             className="mt-2 underline underline-offset-2"
-                            onClick={() => handleToggle(index)} // Sending to handleToggle the description index corresponding to the description of which we DON'T want details
+                            onClick={() => handleToggle(background.key, index)} // Sending both background.key and details index to ensure the button corresponds to the right li
                           >
                             {less}
                           </button>
