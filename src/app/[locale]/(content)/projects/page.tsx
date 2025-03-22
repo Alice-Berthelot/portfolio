eec2164ca0074e2e70a83ed660ef34d451aa4ea9
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-03-22 11:19:05 +0100

    changed project page description, added pictures to girondin project

--- src/app/[locale]/(content)/projects/page.tsx
+++ src/app/[locale]/(content)/projects/page.tsx
@@ -25,7 +25,10 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
       </ScrollDownAnimation>
 
       <ScrollDownAnimation>
-        <p className="text-lg text-ghost-white/75">{t("description")} </p>
+        <section className="text-lg text-ghost-white/75">
+          <p>{t("description-first-p")} </p>
+          <p className="italic">{t("description-second-p")} </p>
+        </section>
       </ScrollDownAnimation>
 
       <section className="mt-6 mb-20 flex flex-col md:flex-row gap-8 lg:gap-0 md:gap-x-12 lg:gap-x-28 justify-center items-center lg:items-auto w-full flex-wrap">
@@ -39,16 +42,20 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
               >
                 <ChangingImage images={project.images} url={url} alt={alt} />
                 <article className="px-10 pt-8 lg:pt-[-4rem] pb-8">
-                <ScrollDownAnimation>
-                  <Subtitle title={project.title} />
+                  <ScrollDownAnimation>
+                    <Subtitle title={project.title} />
                   </ScrollDownAnimation>
                   <ScrollDownAnimation>
-                  <p className="pb-6">{project.description}</p>
+                    <p className="pb-6">{project.description}</p>
                   </ScrollDownAnimation>
                   <ScrollDownAnimation>
-                  <div className="flex justify-end">
-                    <AnimatedLink text={t("more-details")} link={project.url} target={"_blank"}/>
-                  </div>
+                    <div className="flex justify-end">
+                      <AnimatedLink
+                        text={t("more-details")}
+                        link={project.url}
+                        target={"_blank"}
+                      />
+                    </div>
                   </ScrollDownAnimation>
                 </article>
               </article>
